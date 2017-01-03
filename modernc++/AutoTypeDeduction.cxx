615deeb6899d779b5ede86b5d0c00f13bf190306
edros <eduardrostomyan@Eduards-MacBook-Pro.local>
2017-01-04 03:48:53 +0400

    Modified

--- modernc++/AutoTypeDeduction.cxx
+++ modernc++/AutoTypeDeduction.cxx
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <typeinfo>
 using namespace std;
 
 int main()
 {
+ 	const char name[] =  "R. N. Briggs";
+	auto arr1 = name; 
+	auto& arr2 = name;
+	auto&& arr3 = name;
+
+//	std::cout<<typeid(name).name()<<std::endl;
+//	std::cout<<typeid(arr1).name()<<std::endl;
+//	std::cout<<typeid(arr2).name()<<std::endl;
+//	std::cout<<typeid(arr3).name()<<std::endl;
+
+
+	auto x = 27;
+	const auto cx = x;
+	const auto& rx = x;
+
+	auto&& uref1 = x;
+	auto&& uref2 = cx;
+	auto&& uref3 = 27;
+
+	std::cout<<typeid(uref1).name()<<std::endl;
+	std::cout<<typeid(uref2).name()<<std::endl;
+	std::cout<<typeid(uref3).name()<<std::endl;
 }
